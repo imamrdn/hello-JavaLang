be5c0b4996e3a2a0472f37e586206574af2b3cb6
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-25 22:37:43 +0700

    add src mobil, kereta dan motor

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,11 +1,16 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.javafundamental.basic.kendaraan.*;
 import com.dicoding.javafundamental.basic.musik.Gitar;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
 
         Gitar.bunyi();
+
+        Mobil.jumlahBan();
+        Motor.jumlahBan();
+        Kereta.jumlahBan();
     }
 }
