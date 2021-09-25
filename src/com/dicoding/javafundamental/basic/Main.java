a2d095ff34394214c6ebbe859c24982bd5aa7ded
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-25 22:16:14 +0700

    add packages

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,7 +1,11 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        Gitar.bunyi();
     }
 }
