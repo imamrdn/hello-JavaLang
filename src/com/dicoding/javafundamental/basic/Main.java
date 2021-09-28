5e501ede83776eb31aec59289dad850c7669010d
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-28 07:29:54 +0700

    add classpath

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,7 +1,12 @@
 package com.dicoding.javafundamental.basic;
 
-import com.dicoding.javafundamental.basic.kendaraan.*;
+import com.dicoding.javafundamental.basic.kendaraan.Kereta;
+import com.dicoding.javafundamental.basic.kendaraan.Mobil;
+import com.dicoding.javafundamental.basic.kendaraan.Motor;
 import com.dicoding.javafundamental.basic.musik.Gitar;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +17,11 @@ public class Main {
         Mobil.jumlahBan();
         Motor.jumlahBan();
         Kereta.jumlahBan();
+
+        Date today = new Date();
+        System.out.println("Hari ini = " + today);
+        Date tomorrow = DateUtils.addDays(today, 1);
+        System.out.println("Besok = " + tomorrow);
     }
+
 }
