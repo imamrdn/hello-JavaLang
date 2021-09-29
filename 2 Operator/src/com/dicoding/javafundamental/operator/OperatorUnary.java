7d2b37975172f4c8503508ba502fa6f42ab4ae15
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-29 09:50:55 +0700

    operator unary plus

---
+++ 2 Operator/src/com/dicoding/javafundamental/operator/OperatorUnary.java
@@ -0,0 +1,12 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorUnary {
+    public static void main(String[] args){
+        System.out.println("Operator Unary Plus");
+        int nilaiAwal = 5;
+        int hasil = +nilaiAwal;
+        System.out.println("Hasil +5 = " + hasil);
+
+    
+    }
+}
