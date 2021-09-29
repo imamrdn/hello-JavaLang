4cce4b1d776b7d0ed7149a5a17eb6c6092fd8f88
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-29 09:14:24 +0700

    add penjumlahan

---
+++ 2 Operator/src/com/dicoding/javafundamental/operator/OperatorAritmatika.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.operator;
+
+public class OperatorAritmatika {
+    public static void main(String[] args){
+        System.out.println("Operator Penjumlahan");
+        int hasilPenjumlahan = 5+1;
+        System.out.println("Hasil 5 + 1 adalah "+ hasilPenjumlahan);
+        System.out.println();
+    }
+}
