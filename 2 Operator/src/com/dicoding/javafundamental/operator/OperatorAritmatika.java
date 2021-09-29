61d339bfe8f5b613ffac0215cf5fdd2778c4e962
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-29 09:20:16 +0700

    add pengurangan dan pembagian

--- 2 Operator/src/com/dicoding/javafundamental/operator/OperatorAritmatika.java
+++ 2 Operator/src/com/dicoding/javafundamental/operator/OperatorAritmatika.java
@@ -6,5 +6,15 @@ public class OperatorAritmatika {
         int hasilPenjumlahan = 5+1;
         System.out.println("Hasil 5 + 1 adalah "+ hasilPenjumlahan);
         System.out.println();
+
+        System.out.println("Operator Pengurangan");
+        int hasilPengurangan = 4- 1;
+        System.out.println("Hasil dari 4 - 1 adalah "+ hasilPengurangan);
+        System.out.println();
+
+        System.out.println("Operator Pembagian");
+        int hasilPembagian = 20/2;
+        System.out.println("Hasil dari 20 / 4 adalah "+ hasilPembagian);
+        System.out.println();
     }
 }
