d2677e25efb6ba1c9d73d8ce621e7fb0ae41e4af
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-01 17:01:42 +0700

    userOutput

---
+++ 3 Input Output/src/com/dicoding/javafundamental/inputoutput/userOutput.java
@@ -0,0 +1,10 @@
+package com.dicoding.javafundamental.inputoutput;
+
+public class userOutput {
+    public static void main(String[] args){
+        System.out.println("Dicetak pakai println()");
+        System.out.println("Ini juga dicetak dengan println()");
+        System.out.print("Ini dicetak dengan print(), ");
+        System.out.print("Ini dicetak dengan print() juga");
+    }
+}
