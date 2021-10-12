d0feb2064516cf658c63200b8f0420aa382a9ce1
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-12 08:37:37 +0700

    modified switch case java

--- 5 Percabangan/src/com/dicoding/javafundamental/percabangan/Switch.java
+++ 5 Percabangan/src/com/dicoding/javafundamental/percabangan/Switch.java
@@ -1,2 +1,21 @@
-package com.dicoding.javafundamental.percabangan;public class Switch {
+package com.dicoding.javafundamental.percabangan;
+
+public class Switch {
+    public static void main(String[] args){
+        int input = 3;
+
+        switch (input){
+            case 1 :
+                System.out.println("1");
+                break;
+            case 2 :
+                System.out.println("2");
+                break;
+            case 3 :
+                System.out.println("3");
+                break;
+            default:
+                break;
+        }
+    }
 }
