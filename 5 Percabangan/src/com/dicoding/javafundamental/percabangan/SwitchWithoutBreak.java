6ffbb233d100690026158d553c81572faeed4fa2
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-12 08:44:23 +0700

    switch case java without break

---
+++ 5 Percabangan/src/com/dicoding/javafundamental/percabangan/SwitchWithoutBreak.java
@@ -0,0 +1,21 @@
+package com.dicoding.javafundamental.percabangan;
+
+public class SwitchWithoutBreak {
+    public static void main(String[] args){
+        char input = 'C';
+        int output = 0;
+
+        switch(input){
+            case 'A' :
+                output++;
+            case 'B' :
+                output++;
+            case 'C' :
+                output++;
+            default :
+                output++;
+        }
+
+        System.out.println("Nilai akhirnya adalah " + output);
+    }
+}
