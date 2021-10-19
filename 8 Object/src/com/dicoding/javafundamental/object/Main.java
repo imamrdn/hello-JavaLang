c1dca43e23acc845d0690caa455453c454b65e8b
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-19 10:11:28 +0700

    object main

---
+++ 8 Object/src/com/dicoding/javafundamental/object/Main.java
@@ -0,0 +1,9 @@
+package com.dicoding.javafundamental.object;
+
+public class Main {
+    public static void main(String[] args){
+        //menciptakan object  dengan nama 'obyekHewan'
+        Hewan obyekHewan = new Hewan();
+        obyekHewan.cetakNama("Elang");
+    }
+}
