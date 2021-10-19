985d0576ea1d07eb697a55a6f99406c0cd3538f4
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-19 10:27:57 +0700

    modified main object

--- 8 Object/src/com/dicoding/javafundamental/object/Main.java
+++ 8 Object/src/com/dicoding/javafundamental/object/Main.java
@@ -1,9 +1,16 @@
-package com.dicoding.javafundamental.object;
+package com.dicoding.javafundamental.obyek;
 
 public class Main {
-    public static void main(String[] args){
-        //menciptakan object  dengan nama 'obyekHewan'
-        Hewan obyekHewan = new Hewan();
-        obyekHewan.cetakNama("Elang");
+    public static void main(String[] args) {
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+
+        Kucing.beratHewan(2);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
     }
 }
